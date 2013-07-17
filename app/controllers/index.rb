8cb7a6f8a3ff3441bdf812bf7ad7b55b88358747
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 11:25:53 -0500

    add user authentication, create routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,8 +7,31 @@ post '/create_user' do
   p params
   @user = User.new(params[:user])
   if @user.save
-    erb :user_profile
+    session[:user_id] = @user.id
+    redirect to "/user_profile/#{@user.id}"
   else
     redirect to '/'
   end
+end
+
+post '/login' do
+  @user = User.authenticate(params[:user][:email], params[:user][:password])
+  if @user
+    session[:user_id] = @user.id
+    redirect to "/user_profile/#{@user.id}"
+  else
+    @error = 'Invalid Login Info'
+  end
+end
+
+get '/user_profile/:user_id' do 
+  @user = User.find(params[:user_id]) 
+  if @user.id == current_user.id
+    erb :user_profile
+  else
+    @error = "Page not found... Your mom."
+    erb :user_profile
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   validates :email, :presence => true, :uniqueness => true
   validates :password, :presence => true
 
+  # e.g., User.authenticate('jesse@devbootcamp.com', 'apples123')
+  def self.authenticate(email, password)
+     user = self.find_by_email(email)
+     if user.password == password
+      user
+    else
+      nil
+    end
+  end
 end
+
